0e8d7bc258bd5da4644c435358e7b966dbf6b0f2
Ed Bartosh <eduard.bartosh@intel.com>
2012-07-19 10:14:24 +0300

    tester.sh: Updated from the Jenkins job.

--- tester.sh
+++ tester.sh
@@ -1,39 +1,45 @@
-#!/bin/sh -efu
+set -eu
 
 PROJECT=`basename $(pwd)`
-BUILDHOME=./buildroot/home/build/
-PACKAGE=`echo $JOB_NAME|cut -f2- -d-|cut -f1 -d/`
+BUILDROOT=../buildroot-$BUILD_NUMBER
+BUILDHOME=$BUILDROOT/home/build
+PACKAGE=`echo $GERRIT_PROJECT|cut -f1 -d-`
+OBS_PROJECT=home:tester:Tools-$PACKAGE-$BUILD_NUMBER
+
+# Clean buildroot on exit
+trap "sudo rm -rf $BUILDROOT; exit" INT TERM EXIT
 
 # Submit packages to OBS
-make -C packaging/ all
-# TODO: Fix this build: build-package --sproject Tools:Devel --tproject home:tester:Tools-$BUILD_NUMBER --package $PACKAGE packaging/*
-build-package --sproject home:tester:Tools --tproject home:tester:Tools-$PACKAGE-$BUILD_NUMBER --package $PACKAGE packaging/*
-
-cd ..
+if test "${CAN_SUBMIT_TO_OBS+defined}" ; then
+    make -C packaging/ all
+    build-package --sproject home:tester:Tools --tproject $OBS_PROJECT --package $PACKAGE packaging/*
+else
+    build-package --wait --tproject $OBS_PROJECT
+fi
 
 # re-create directory for reports
 rm -rf $WORKSPACE/reports
 mkdir $WORKSPACE/reports
 
 # Unpack buildroot.tar
-sudo rm -rf ./buildroot
 sudo tar -xf ~/buildroot.tar
+sudo mv buildroot $BUILDROOT
 
-mkdir -p /home/build/reports/
+# Get list of built binary packages
+osc ls -b $OBS_PROJECT -r $OBS_REPO -a $OBS_ARCH > $WORKSPACE/packages.list
+PACKAGES=`sed -n 's/^\(.*\)\.\(deb\|rpm\)$/\1/p' $WORKSPACE/packages.list|tr '\n' ' '`
 
 # Install packages
-sudo chroot ./buildroot /usr/bin/install_package $PACKAGE $BUILD_NUMBER /home/build/reports/install.log
+sudo chroot $BUILDROOT /usr/bin/install_package Tools-$PACKAGE-$BUILD_NUMBER $OBS_REPO "$PACKAGES"
 
 # copy source tree to buildroot
-cp -a ./$PROJECT $BUILDHOME
+cp -a ../$PROJECT $BUILDHOME/
 
 # Run tests
-sudo chroot ./buildroot /usr/bin/run_tests /home/build/$PROJECT /home/build/reports/ /home/build/reports/pylint.log
+sudo chroot $BUILDROOT /usr/bin/run_tests /home/build/$PROJECT /home/build/reports/
 
 # Copy test results
-cp $BUILDHOME/reports/* $WORKSPACE/reports
-
-# Clean buildroot
-sudo rm -rf ./buildroot
+ls -la $BUILDHOME/reports/
+cp $BUILDHOME/reports/* $WORKSPACE/reports/
 
 echo 'done'
