eeda62405877d24956e20cd3c3f1ea8ab775fa2c
Olev Kartau <olev.kartau@intel.com>
2012-09-18 14:35:44 +0300

    Added code to delete buildroots under workspaces
    Change-Id: Ibc9ac23f6210cf36ac21da791dc97065c630cf0b

--- otc-tools-tester-maintenance-del-old-buildroots.sh
+++ otc-tools-tester-maintenance-del-old-buildroots.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # DAYS_TO_KEEP is Jenkins job argument
-# older than that (not changed during last DAYS_TO_KEEP days) buildroots
-# will be deleted
+# older than that (not changed during DAYS_TO_KEEP days)
+# buildroots are deleted
+
+# 1st pass, delete buildroots under /tmp/
+# There is sudo rule to delete them
 
 TO_DEL=`find /tmp -maxdepth 1 -type d -ctime +$DAYS_TO_KEEP \
  -name 'buildroot-updated-*'`
@@ -11,10 +14,14 @@ for BR in ${TO_DEL}; do
   sudo rm -rf $BR
 done
 
-TO_DEL=`find $JENKINS_HOME/workspace/*/label -maxdepth 1 \
- -type d -ctime +$DAYS_TO_KEEP -name 'buildroot-*'`
+# 2nd pass, delete buildroots under Jenkins workspaces.
+# We need to be in label/$NODE_NAME dir to make rm work by sudo rule
 
-for BR in ${TO_DEL}; do
-  echo $BR
+find $JENKINS_HOME/workspace/ -maxdepth 1 -mindepth 1 -type d | \
+while read PROJ; do
+  if [ -d "$PROJ/label/$NODE_NAME" ]; then
+    cd "$PROJ/label/$NODE_NAME"
+    find .. -maxdepth 1 -type d -ctime +$DAYS_TO_KEEP \
+      -name 'buildroot-*' -exec sudo rm -rf {} \;
+  fi
 done
-
