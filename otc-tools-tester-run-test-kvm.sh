ddfd59045afa82652eb23dba3011005830117e47
Olev Kartau <olev.kartau@intel.com>
2013-01-21 14:27:12 +0200

    Properly delete KVM_ROOT directory after test run
    KVM_ROOT was left around as garbage, because there was
    no removal command. The attempt to remove was done before
    creation, but as name is unique to run (using BUILD_NUMBER),
    this removal name never matched anything.
    
    Change-Id: I5bdd7b762bc62901c59e0b5c248e11cd2cc1456c

--- otc-tools-tester-run-test-kvm.sh
+++ otc-tools-tester-run-test-kvm.sh
@@ -5,6 +5,7 @@ UMOUNT="sudo umount -l"
 Cleanup () {
  mountpoint -q $BUILDHOME && $UMOUNT $BUILDHOME
  mountpoint -q $KVM_ROOT && $UMOUNT $KVM_ROOT
+ rm -fr $KVM_ROOT
  rm -fr $SRC_TMPCOPY
 }
 
@@ -94,8 +95,7 @@ fi
 # prepare KVM disks and mount KVM home
 KVM_HDA="$KVM_ROOT/kvm-hda-$OBS_REPO"
 KVM_HDB="$KVM_ROOT/kvm-hdb"
-rm -rf $KVM_ROOT
-mkdir -m 777 $KVM_ROOT
+mkdir -p -m 777 $KVM_ROOT
 sudo mount -t tmpfs -o size=8G tmpfs $KVM_ROOT
 cp --sparse=always /var/lib/jenkins/kvm-seed-hda-$OBS_REPO $KVM_HDA
 cp --sparse=always /var/lib/jenkins/kvm-seed-hdb $KVM_HDB
