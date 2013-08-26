5bbeb8b422f41f0350ba5ea3f8b5b1f529d5c0a2
Huang Hao <hao.h.huang@intel.com>
2013-08-26 19:00:26 +0800

    Verify changes of itest cases.
    When a change is made in itest-cases-gbs project, tester job
    otc-tools-tester-itest-cases-gbs will build package for this
    change. Then trigger_itest_verify.sh can trigger itest job to
    install package from tester's temp repo and verify modified
    test cases.
    
    Script trigger_itest_verify.sh will generate a file named
    "trigger.env" which contains a variable "RUN_ITEST_KVM_ARGV" in
    format like "-p xx -p xx -t xx". It will pass to itest job as
    an environment variable.
    
    Script run-itest-kvm.sh will be written in jenkins job config in
    format as "run-itest-kvm.sh -p default_package -p another_package
    -t default_test_suite $RUN_ITEST_KVM_ARGV". When it's triggered
    by time, it will use the default package and suite. When it's
    triggered by cases changes RUN_ITEST_KVM_ARGV can overwrite the
    default values.
    
    Fixes: #1214
    Change-Id: If4ac991bbe618950b41353b651a6d28e4891d34f

--- run-itest-kvm.sh
+++ run-itest-kvm.sh
@@ -9,22 +9,54 @@
 #cd $WORKSPACE/run-itest
 #label=openSUSE_12.1-i586-debug
 
-
 additional_init() {
     BUILDHOME=$1
 
-    cat >>$BUILDHOME/run <<EOF
-$TARGETBIN/install_package "" "$OBS_REPO" "" "itest" "itest-core" ""
-$TARGETBIN/install_package "" "$OBS_REPO" "" "Tools:/Devel" "gbs" ""
-$TARGETBIN/install_package "" "$OBS_REPO" "" "itest:/Devel" "itest-cases-gbs" ""
+    i=0
+    while [ $i -lt $install_package_cnt ]; do
+        pack=${install_package_name[$i]}
+        proj=${install_package_proj[$i]}
+        i=$(expr $i + 1)
+        cat >>$BUILDHOME/run <<EOF
+$TARGETBIN/install_package "" "$OBS_REPO" "" "$proj" "$pack" ""
+EOF
+    done
 
-su - build -c "cd /srv/itest/cases/gbs; runtest -v 2>&1"
+    cat >>$BUILDHOME/run <<EOF
+su - build -c "cd $itest_env_path; runtest -v $test_suite 2>&1"
 EOF
 }
 
 usage() {
-  echo "Usage: [ -s NAME_SUFFIX]"
-  exit 0
+    echo "Usage: itest_env_path [options]"
+    echo "    itest_env_path: path contain test cases"
+    echo "    -p project,package: package to install in vm"
+    echo "    -t test_suite: test to run, comma separated"
+    echo "    -s NAME_SUFFIX"
+}
+
+install_package_proj=
+install_package_name=
+install_package_cnt=0
+add_pack() {
+    proj=$1
+    pack=$2
+
+    # overwrite project if package exists
+    i=0
+    while [ $i -lt $install_package_cnt ]; do
+        packi=${install_package_name[$i]}
+        if [ $packi = $pack ]; then
+            install_package_proj[$i]=$proj
+            return
+        fi
+        i=$(expr $i + 1)
+    done
+
+    # append new pair if package doesn't exist
+    install_package_proj[$install_package_cnt]=$proj
+    install_package_name[$install_package_cnt]=$pack
+    install_package_cnt=$(expr $install_package_cnt + 1)
 }
 
 ############
@@ -32,11 +64,27 @@ usage() {
 ############
 
 NAME_SUFFIX=""
-set -- $(getopt hs: "$@")
+test_suite=
+
+set -- $(getopt hs:p:t: "$@")
 while [ $# -gt 0 ]
 do
     case "$1" in
-    (-h) usage;;
+    (-h) usage; exit 0;;
+    (-p)
+        if [ 2 -ne $(echo $2|awk -F',' '{print NF}') ]; then
+            echo "Bad package name and project name to install:$2"
+            exit 1
+        fi
+        proj=$(echo $2|awk -F',' '{print $1}')
+        pack=$(echo $2|awk -F',' '{print $2}')
+        add_pack $proj $pack
+        shift
+        ;;
+    (-t)
+        test_suite="$(echo $2|tr , ' ')"
+        shift
+        ;;
     (-s) NAME_SUFFIX=$2; shift;;
     (--) shift; break;;
     (-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
@@ -44,6 +92,16 @@ do
     esac
     shift
 done
+itest_env_path=$1
+if [ -z "$itest_env_path" ]; then
+    echo "Argument itest_env_path is required"
+    exit 1
+fi
+
+if [ $install_package_cnt -lt 1 ]; then
+    echo "Can't find test case and target package, please give it by -p"
+    exit 1
+fi
 
 OBS_REPO=`echo $label|cut -f1 -d-`
 OBS_ARCH=`echo $label|cut -f2 -d-`
