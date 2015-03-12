e25e3ff8ec1b7df0f265f0ed2a1cc45596cbe7ed
Olev Kartau <olev.kartau@intel.com>
2015-03-12 14:15:43 +0200

    Fix and improve reporting of distro release
    Some distros had version reported with dot and minor number,
    which is not what we need. Also, CentOS needs separate
    treatment for versions 6 and 7.
    Arch will have just zero without minor number.
    
    Change-Id: Ibc7402d7dfa46bd85edd75f46ed47c437fca4dfd

--- otc-tools-tester-system-what-release.sh
+++ otc-tools-tester-system-what-release.sh
@@ -11,18 +11,29 @@ if [ -f /etc/lsb-release ]; then
     . /etc/lsb-release
     REL=$DISTRIB_ID
     VER=$DISTRIB_RELEASE
+elif [ -f /etc/centos-release ]; then
+    REL=`cat /etc/centos-release | cut -d' ' -f 1`
+    # Centos 6 does not have /etc/os-release
+    # centos-release has different number of fields in 6 vs 7
+    if [ -f /etc/os-release ]; then
+      # this covers CentOS_7
+      VER=`grep VERSION_ID /etc/os-release | tr -d '[:punct:][:alpha:]'`
+    else
+      # this covers CentOS_6, have to strip off 6.x part after dot
+      VER=`cat /etc/centos-release | cut -d' ' -f 3 | cut -d'.' -f 1`
+    fi
 elif [ -f /etc/debian_version ]; then
     REL=Debian
-    VER=$(cat /etc/debian_version)
+    VER=`grep VERSION_ID /etc/os-release | tr -d '[:punct:][:alpha:]'`
 elif [ -f /etc/redhat-release ]; then
     REL=`cat /etc/redhat-release | cut -d' ' -f 1`
-    VER=`cat /etc/redhat-release | cut -d' ' -f 3`
+    VER=`grep VERSION_ID /etc/os-release | tr -d '[:punct:][:alpha:]'`
 elif [ -f /etc/SuSE-release ]; then
     REL=`cat /etc/SuSE-release | head -1 | cut -d' ' -f 1`
     VER=`cat /etc/SuSE-release | head -1 | cut -d' ' -f 2`
 elif [ -f /etc/arch-release ]; then
     REL="Arch"
-    VER="0.0"
+    VER="0"
 fi
 
 echo "$REL-$VER"
